05e965bcb79229b905e80ed2ecf2d4fdaba77431
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-05-01 23:51:04 +0100

    wip

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -8,12 +8,8 @@
       @change="searchMeals"
     />
   </div>
-  <div>
-    <div
-      v-for="meal of meals"
-      :key="meal.idMeal"
-      class="grid grid-cols-1 md:grid-cols-3"
-    >
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-3">
+    <div v-for="meal of meals" :key="meal.idMeal">
       <img :src="meal.strMealThumb" :alt="strMeal" />
       <h3>{{ meal.strMeal }}</h3>
       <div>
