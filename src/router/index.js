3138b914e8cde1b549a9f1b3acf495e9fe584ff1
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-05-02 16:01:22 +0100

    wip

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,10 @@ import { createRouter, createWebHistory } from "vue-router";
 import DefaultLayout from "../components/DefaultLayout.vue";
 import GuestLayout from "../components/GuestLayout.vue";
 import Home from "../views/Home.vue";
-import MealsByName from "../views/MealsByName.vue";
-import MealsByLetter from "../views/MealsByLetter.vue";
 import MealsByIngredient from "../views/MealsByIngredient.vue";
+import MealsByLetter from "../views/MealsByLetter.vue";
+import MealsByName from "../views/MealsByName.vue";
+import MealDetails from "../views/MealDetails.vue";
 
 const routes = [
   {
@@ -34,7 +35,7 @@ const routes = [
       {
         path: "/meals/:id",
         name: "mealDetails",
-        component: mealDetails,
+        component: MealDetails,
       },
     ],
   },
--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -1,3 +1,21 @@
-<template></template>
+<template>
+  <div>
+    <h1 class="text-5xl font-bold mb-5">{{ meal.strMeal }}</h1>
+    <img :src="meal.strMealThumb" :alt="meal.strMeal" />
+  </div>
+</template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import axiosClient from "../axiosClient";
+
+const route = useRoute();
+const meal = ref({});
+
+onMounted(() => {
+  axiosClient.get(`lookup.php?i=${route.params.id}`).then(({ data }) => {
+    meal.value = data.meals[0] || {};
+  });
+});
+</script>
