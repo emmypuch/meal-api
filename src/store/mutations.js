10aa32265cd0888c7cde994e0a2610e2fad4468b
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-05-02 13:42:14 +0100

    fixed bug

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,3 +1,3 @@
 export function setSearchedMeals(state, meals) {
-  state.setSearchedMeals = meals;
+  state.searchedMeals = meals;
 }
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -8,7 +8,7 @@
       @change="searchMeals"
     />
   </div>
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-3">
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-3 p-8 bg-red-200">
     <div v-for="meal of meals" :key="meal.idMeal">
       <img :src="meal.strMealThumb" :alt="strMeal" />
       <h3>{{ meal.strMeal }}</h3>
@@ -22,12 +22,16 @@
 
 <script setup>
 import { computed } from "@vue/reactivity";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 
 import store from "../store";
 
 const keyword = ref("");
 const meals = computed(() => store.state.searchedMeals);
+
+// onMounted(() => {
+//   searchMeals();
+// });
 
 function searchMeals() {
   store.dispatch("searchMeals", keyword.value);
