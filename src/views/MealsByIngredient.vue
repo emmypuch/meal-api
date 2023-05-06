3eb43d04edd5edfca672943aa438e63cc2f5e9e2
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-05-06 01:46:40 +0100

    wip

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="p-8">
+  <Meals :meals="meals" />
+  <!-- <div class="p-8">
     <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
     <router-link
       :to="{
@@ -13,18 +14,29 @@
       <h3 class="text-2xl font-bold mb-2">{{ ingredient.strIngredient }}</h3>
       <p>{{ ingredient.strDescription }}</p>
     </router-link>
-  </div>
+  </div> -->
 </template>
 
 <script setup>
+import { computed } from "@vue/reactivity";
 import { onMounted, ref } from "vue";
-import axiosClient from "/src/axiosClient.js";
+import { useRoute } from "vue-router";
+import store from "../store";
+import Meals from "../components/Meals.vue";
+// import axiosClient from "/src/axiosClient.js";
 
-const ingredients = ref([]);
+// const ingredients = ref([]);
+
+// onMounted(() => {
+//   axiosClient.get("list.php?i=list").then(({ data }) => {
+//     ingredients.value = data.meals;
+//   });
+// });
+
+const route = useRoute();
+const meals = computed(() => store.state.mealsByIngredient);
 
 onMounted(() => {
-  axiosClient.get("list.php?i=list").then(({ data }) => {
-    ingredients.value = data.meals;
-  });
+  store.dispatch("searchMealsByIngredient", route.params.ingredient);
 });
 </script>
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -8,19 +8,15 @@
       @change="searchMeals"
     />
   </div>
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-    <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
-  </div>
+  <Meals :meals="meals" />
 </template>
 
 <script setup>
 import { computed } from "@vue/reactivity";
-
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import store from "../store";
-import YouTubeButton from "../components/YouTubeButton.vue";
-import MealItem from "../components/MealItem.vue";
+import Meals from "../components/Meals.vue";
 
 const route = useRoute();
 const keyword = ref("");
