57a73d602a68cf1c0c18f45dc00ce9f2407e84f4
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-05-07 00:08:42 +0100

    fixed error

--- src/main.js
+++ src/main.js
@@ -5,5 +5,7 @@ import "./style.css";
 import App from "./App.vue";
 import * as filters from "./filters";
 
-createApp(App).use(router).use(store).mount("#app");
+const app = createApp(App);
+app.use(router).use(store).mount("#app");
+
 app.config.globalProperties.$filters = filters;
