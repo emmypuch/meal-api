212b98fdfe9b5ab1ed3be5c55036e00503674acc
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-05-06 22:30:28 +0100

    modified views

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="p-8">
     <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded border-2 bg-white border-gray-200 w-full mb-3"
+      placeholder="Search Ingredients"
+    />
     <router-link
       :to="{
         name: 'byIngredient',
         params: { ingredient: ingredient.strIngredient },
       }"
-      v-for="ingredient of ingredients"
+      v-for="ingredient of computedIngredients"
       :key="ingredient.id"
       class="block bg-white rounded p-3 mb-3 shadow"
     >
@@ -17,10 +23,18 @@
 </template>
 
 <script setup>
+import { computed } from "@vue/reactivity";
 import { onMounted, ref } from "vue";
 import axiosClient from "/src/axiosClient.js";
 
+const keyword = ref("");
 const ingredients = ref([]);
+const computedIngredients = computed(() => {
+  if (!computedIngredients) return ingredients;
+  return ingredients.value.filter((i) =>
+    i.strIngredient.toLowerCase().includes(keyword.value.toLowerCase())
+  );
+});
 
 onMounted(() => {
   axiosClient.get("list.php?i=list").then(({ data }) => {
--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -8,16 +8,14 @@
       {{ letter }}
     </router-link>
   </div>
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-    <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
-  </div>
+  <Meals :meals="meals" />
 </template>
 
 <script setup>
 import { computed } from "@vue/reactivity";
 import { onMounted, watch } from "vue";
 import { useRoute } from "vue-router";
-import MealItem from "../components/MealItem.vue";
+import Meals from "../components/Meals.vue";
 import store from "../store/index.js";
 
 const route = useRoute();
